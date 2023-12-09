6e38daa883da2a891c8fe607792e7b18a8830829
qnaiv <43468715+qnaiv@users.noreply.github.com>
2023-12-09 18:02:45 +0900

    コンポーネント分けリファクタリング

---
+++ src/components/Status.js
@@ -0,0 +1,9 @@
+export default function Status({ winner, xIsNext }) {
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+    return <div className="status">{status}</div>;
+}
